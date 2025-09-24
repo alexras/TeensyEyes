e36ea3c116c5318d87c729642be690be1219ff38
Alex Rasmussen <alex.c.rasmussen@gmail.com>
2025-09-23 21:52:04 -0700

    Don't mirror the left and right eyes. Add a Blink pin. Remove other eye configs from runtime

--- src/config.h
+++ src/config.h
@@ -3,29 +3,31 @@
 #include "eyes/eyes.h"
 
 // Enable the eye(s) you want to #include -- these are large graphics tables for various eyes:
-//#include "eyes/240x240/anime.h"
-#include "eyes/240x240/bigBlue.h"
+// #include "eyes/240x240/anime.h"
+// #include "eyes/240x240/bigBlue.h"
 //#include "eyes/240x240/blueFlame1.h"
 //#include "eyes/240x240/blueFlame2.h"
-#include "eyes/240x240/brown.h"
-#include "eyes/240x240/cat.h"
-#include "eyes/240x240/demon.h"
-#include "eyes/240x240/doe.h"
-#include "eyes/240x240/doomRed.h"
-#include "eyes/240x240/doomSpiral.h"
-#include "eyes/240x240/dragon.h"
+// #include "eyes/240x240/brown.h"
+// #include "eyes/240x240/cat.h"
+// #include "eyes/240x240/demon.h"
+// #include "eyes/240x240/doe.h"
+// #include "eyes/240x240/doomRed.h"
+// #include "eyes/240x240/doomSpiral.h"
+// #include "eyes/240x240/dragon.h"
 //#include "eyes/240x240/firebox.h"
 //#include "eyes/240x240/fish.h"
-#include "eyes/240x240/fizzgig.h"
+// #include "eyes/240x240/fizzgig.h"
 //#include "eyes/240x240/flame.h"
-//#include "eyes/240x240/hazel.h"
-#include "eyes/240x240/hypnoRed.h"
+// #include "eyes/240x240/hazel.h"
+// #include "eyes/240x240/hypnoRed.h"
 //#include "eyes/240x240/leopard.h"
 //#include "eyes/240x240/newt.h"
-#include "eyes/240x240/skull.h"
-#include "eyes/240x240/snake.h"
+// #include "eyes/240x240/skull.h"
+// #include "eyes/240x240/snake.h"
 //#include "eyes/240x240/spikes.h"
-#include "eyes/240x240/toonstripe.h"
+// #include "eyes/240x240/toonstripe.h"
+#include "eyes/240x240/magpie.h"
+
 
 #include "eyes/EyeController.h"
 
@@ -45,28 +47,29 @@
 // A list of all the different eye definitions we want to use
 std::array<std::array<EyeDefinition, 2>, 13> eyeDefinitions{{
 //                                                               {anime::left, anime::right},
-                                                               {bigBlue::eye, bigBlue::eye},
+                                                               // {bigBlue::eye, bigBlue::eye},
 //                                                               {blueFlame1::eye, blueFlame1::eye},
 //                                                               {blueFlame2::eye, blueFlame2::eye},
-                                                               {brown::eye, brown::eye},
-                                                               {cat::eye, cat::eye},
-                                                               {demon::left, demon::right},
-                                                               {doe::left, doe::right},
-                                                               {doomRed::eye, doomRed::eye},
-                                                               {doomSpiral::left, doomSpiral::right},
-                                                               {dragon::eye, dragon::eye},
+                                                              //  {brown::eye, brown::eye},
+                                                                // {cat::eye, cat::eye},
+                                                              //  {demon::left, demon::right},
+                                                              //  {doe::left, doe::right},
+                                                              //  {doomRed::eye, doomRed::eye},
+                                                              //  {doomSpiral::left, doomSpiral::right},
+                                                               //  {dragon::eye, dragon::eye},
 //                                                               {firebox::eye, firebox::eye},
 //                                                               {fish::eye, fish::eye},
-                                                               {fizzgig::eye, fizzgig::eye},
+                                                              //  {fizzgig::eye, fizzgig::eye},
 //                                                               {flame::eye, flame::eye},
-//                                                               {hazel::eye, hazel::eye},
-                                                               {hypnoRed::eye, hypnoRed::eye},
+                                                               // {hazel::eye, hazel::eye},
+                     //                                           {hypnoRed::eye, hypnoRed::eye},
 //                                                               {leopard::left, leopard::right},
 //                                                               {newt::eye, newt::eye},
-                                                               {skull::eye, skull::eye},
-                                                               {snake::eye, snake::eye},
+                                                              //  {skull::eye, skull::eye},
+                                                              //  {snake::eye, snake::eye},
 //                                                                {spikes::eye, spikes::eye}
-                                                               {toonstripe::eye, toonstripe::eye},
+                                                                // {toonstripe::eye, toonstripe::eye},
+                                                                {magpie::eye, magpie::eye}
                                                            }
 };
 
@@ -81,7 +84,7 @@ std::array<std::array<EyeDefinition, 2>, 13> eyeDefinitions{{
 #ifdef USE_GC9A01A
 GC9A01A_Config eyeInfo[] = {
     // CS DC MOSI SCK RST ROT MIRROR USE_FB ASYNC
-    {0,  2, 26, 27, 3, 0, true,  true, true}, // Left display
+    {0,  2, 26, 27, 3, 0, false,  true, true}, // Left display
     {10, 9, 11, 13, 8, 0, false, true, true}, // Right display
 };
 #elif defined USE_ST7789
@@ -100,7 +103,7 @@ constexpr uint32_t EYE_DURATION_MS{4'000};
 constexpr uint32_t SPI_SPEED{30'000'000};
 
 // Set to -1 to disable the blink button and/or joystick
-constexpr int8_t BLINK_PIN{-1};
+constexpr int8_t BLINK_PIN{32};
 constexpr int8_t JOYSTICK_X_PIN{-1};
 constexpr int8_t JOYSTICK_Y_PIN{-1};
 constexpr int8_t LIGHT_PIN{-1};
